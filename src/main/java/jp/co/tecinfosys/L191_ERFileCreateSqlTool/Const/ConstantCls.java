a85ba39d323db153c5447e7ae55086f7f46d579b
MTT190501 <MTT190501@MTT-N-190501>
2019-06-05 13:24:41 +0900

    マージ

--- src/main/java/jp/co/tecinfosys/L191_ERFileCreateSqlTool/Const/ConstantCls.java
+++ src/main/java/jp/co/tecinfosys/L191_ERFileCreateSqlTool/Const/ConstantCls.java
@@ -4,11 +4,41 @@ public class ConstantCls {
 
 
     public final static String STR_EQUALS = "=";
+    public final static String STR_COMMA = ",";
+    public final static String STR_D_QUOTATION = "\"";
 
     public final static String STR_PNAME = "PName";
     public final static String STR_LNAME = "LName";
     public final static String STR_TABLE_OPTION = "TableOption";
     public final static String STR_FIELD = "Field";
     public final static String STR_NO_DDL = "NoDDL";
+    public final static String STR_Index = "Index";
+
+    public final static String STR_TB_SQL_FORMAT = "IF NOT EXISTS(SELECT * FROM sysobjects WHERE name = '{tablePName}' AND type = 'U')\r\n" +
+                                                         "BEGIN\r\n" +
+                                                         "        create table [{tablePName}] (\r\n" +
+                                                         "          {strColumns}\r\n" +
+                                                         "        ) \r\n" +
+                                                         "\r\n" +
+                                                         "END\r\n" +
+                                                         "GO\r\n" +
+                                                         "\r\n" +
+                                                         "{strIndexs}"+
+                                                         "{tableOption}";
+    public final static String STR_TB_INDEX_SQL_FORMAT = "IF NOT EXISTS(SELECT * FROM sys.indexes WHERE name = '{indexPName}')\r\n" +
+                                                                "BEGIN\r\n" +
+                                                                "        create {indexTypeStr} {indexPName}\r\n" +
+                                                                "          on [{tablePName}]({indexColumn});\r\n" +
+                                                                "END\r\n" +
+                                                                "GO" +
+                                                                "\r\n";
+
+
+
+
+    public final static String STR_CA = "CA";
+    public final static String STR_NA = "NA";
+    public final static String RELATION_CA = "  on delete no action\r\n" + "  on update no action;\r\nEND\r\n" + "GO\r\n";
+    public final static String RELATION_NA = "  on delete CASCADE\r\n" + "  on update CASCADE;\r\nEND\r\n" + "GO\r\n";
 
 }
--- src/main/java/jp/co/tecinfosys/L191_ERFileCreateSqlTool/utils/BeanListCreateUtil.java
+++ src/main/java/jp/co/tecinfosys/L191_ERFileCreateSqlTool/utils/BeanListCreateUtil.java
@@ -8,7 +8,8 @@ import org.apache.commons.lang3.StringUtils;
 import jp.co.tecinfosys.L191_ERFileCreateSqlTool.Const.ConstantCls;
 import jp.co.tecinfosys.L191_ERFileCreateSqlTool.bean.Entity;
 import jp.co.tecinfosys.L191_ERFileCreateSqlTool.bean.Field;
-import jp.co.tecinfosys.L191_ERFileCreateSqlTool.bean.Relation;
+import jp.co.tecinfosys.L191_ERFileCreateSqlTool.bean.Index;
+import jp.co.tecinfosys.L191_ERFileCreateSqlTool.bean.RelationBean;
 
 public class BeanListCreateUtil {
 
@@ -23,13 +24,15 @@ public class BeanListCreateUtil {
 
             Entity entity = new Entity();
             List<Field> fieldList = new ArrayList<Field>();
+            List<Index> indexList = new ArrayList<Index>();
 
             for (String str : strListEntity) {
 
                 Field field = new Field();
-                int indx = StringUtils.indexOf(str, ConstantCls.STR_EQUALS);
-                String key = StringUtils.substring(str, 0, indx);
-                String value = StringUtils.substring(str, indx+1);
+                Index index = new Index();
+                int indexEquals = StringUtils.indexOf(str, ConstantCls.STR_EQUALS);
+                String key = StringUtils.substring(str, 0, indexEquals);
+                String value = StringUtils.substring(str, indexEquals+1);
 
                 switch (key) {
                 case ConstantCls.STR_PNAME:
@@ -43,7 +46,7 @@ public class BeanListCreateUtil {
                     continue;
                 case ConstantCls.STR_FIELD:
                     String[] fields = createFiled(value);
-                    field.setColumnLName(fields[0].replaceFirst("\"", ""));
+                    field.setColumnLName(fields[0].replaceFirst(ConstantCls.STR_D_QUOTATION, ""));
                     field.setColumnPName(fields[1]);
                     field.setColumnType(fields[2]);
                     field.setColumnNULL(fields[3]);
@@ -51,13 +54,22 @@ public class BeanListCreateUtil {
                     field.setColumnDefaultValue(fields[5]);
                     field.setColumnComment(fields[6]);
                     fieldList.add(field);
-                    entity.setFieldList(fieldList);
+                    continue;
+                case ConstantCls.STR_Index:
+                    int i = StringUtils.indexOf(value, ConstantCls.STR_EQUALS);
+                    index.setIndexPName(StringUtils.substring(value, 0, i));
+                    index.setIndexType(StringUtils.substring(value, i+1 , i+2));
+                    index.setIndexColumn(StringUtils.substring(value, i+3));
+                    indexList.add(index);
                     continue;
                 case ConstantCls.STR_NO_DDL:
                     entity.setNoDDL(value);
                 }
 
             }
+
+            entity.setFieldList(fieldList);
+            entity.setIndexList(indexList);
 
             if (StringUtils.isEmpty(entity.getNoDDL())) {
                 entityList.add(entity);
@@ -69,7 +81,7 @@ public class BeanListCreateUtil {
     }
 
 
-    public static List<Relation> createEntityList(List<Entity> entityList, List<List<String>> strList) {
+    public static List<RelationBean> createEntityList(List<Entity> entityList, List<List<String>> strList) {
 
 
         return null;
@@ -80,16 +92,16 @@ public class BeanListCreateUtil {
         String[] str = new String[9];
 
         for (int i=0; i<9; i++) {
-            if(value.startsWith("\"")) {
+            if(value.startsWith(ConstantCls.STR_D_QUOTATION)) {
                 value = StringUtils.substring(value, 1);
-                str[i] = StringUtils.substring(value, 0, value.indexOf("\""));
-                value = StringUtils.substring(value, value.indexOf("\"") + 2);
-            } else if (value.startsWith(",")) {
+                str[i] = StringUtils.substring(value, 0, value.indexOf(ConstantCls.STR_D_QUOTATION));
+                value = StringUtils.substring(value, value.indexOf(ConstantCls.STR_D_QUOTATION) + 2);
+            } else if (value.startsWith(ConstantCls.STR_COMMA)) {
                 str[i] = "";
                 value = StringUtils.substring(value, 1);
             } else {
-                str[i] =StringUtils.substring(value, 0, value.indexOf(","));
-                value = StringUtils.substring(value, value.indexOf(",")+1);
+                str[i] =StringUtils.substring(value, 0, value.indexOf(ConstantCls.STR_COMMA));
+                value = StringUtils.substring(value, value.indexOf(ConstantCls.STR_COMMA)+1);
             }
         }
 
